28667f5beb9657a2c7a1035ed7e7caac1e6df1bb
pablo gutierrez <pabloyucragutierrez@gmail.com>
2026-01-25 23:27:27 -0500

    ccd

--- src/actividades/actividades.module.ts
+++ src/actividades/actividades.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ActividadesService } from './actividades.service';
 import { ActividadesController } from './actividades.controller';
-import { PrismaModule } from '../prisma/prisma.module';
-import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 
 @Module({
-  imports: [PrismaModule, CloudinaryModule],
   controllers: [ActividadesController],
   providers: [ActividadesService],
 })
